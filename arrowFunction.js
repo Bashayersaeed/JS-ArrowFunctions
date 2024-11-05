51dba7b68c76be20322e90154232bd357fc4bfe9
Bashayersaeed <bsaeed83@ymail.com>
2024-11-05 19:54:36 +0300

    corrections done, thank you for the feedback

--- arrowFunction.js
+++ arrowFunction.js
@@ -1,44 +1,44 @@
-//TASK 1 
+// //TASK 1 
 
 const greet =(name) => `Hello, ${name}!`;
 
-console.log(greet("Bashayer"));
+//console.log(greet("Bashayer"));
 
-//TASK 2 
-// Write a simple arrow function that takes two parameters and returns their sum.
+// //TASK 2 
+// // Write a simple arrow function that takes two parameters and returns their sum.
 
-// function add(a,b){
-//     const result = a+b;
-//     return result
-// };
-// const result =add(3,7)
-// console.log(result);
+// // function add(a,b){
+// //     const result = a+b;
+// //     return result
+// // };
+// // const result =add(3,7)
+// // console.log(result);
 
-const add = (a,b) =>  a+b;
-console.log (3+7);
+const add = (a, b) =>  a + b;
+    //console.log (3+7);
 
-//TASK 3
-// Write a simple arrow function that squares a number.
+// //TASK 3
+// // Write a simple arrow function that squares a number.
 
-// square (a,b) => result
-// const square= function (firstNum){
-//     return firstNum * firstNum;
-// };
-// console.log (square(3));
+// // square (a,b) => result
+// // const square= function (firstNum){
+// //     return firstNum * firstNum;
+// // };
+// // console.log (square(3));
 
-const squareNum = (firstNum)=> firstNum * firstNum;
+const squareNum = (number) => number * number;
+   // console.log (squareNum (3));
 
-console.log (squareNum (3));
 
-
-//Challenge
-//[]Create an arrow function that takes an array of numbers and returns a new array with each number squared.
+// //Challenge
+// //[]Create an arrow function that takes an array of numbers and returns a new array with each number squared.
 
 const numbers = [1,2,3,4,5];
-square = numbers.map (function(number){
-    return number * number;
-});
+// square = numbers.map (function(number) {
+//     return number * number;
+// });
 
-console.log(square);
+const square = numbers.map((number) => number * number);
+//console.log(square);
 
 
